75e06b77ad7710f46220247a08eef2a52ce5a058
charles66820 <charles.goedefroit@gmail.com>
2024-06-02 12:39:44 +0200

    add meta card support

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,42 @@ const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
+const launchesData = require("./launches.json");
+function isArchive(date) {
+  const currentDate = new Date();
+  const epsilon2DayTime = 180000000;
+  return currentDate.getTime() - date.getTime() > epsilon2DayTime;
+}
+
+const futurLaunches = launchesData.launches.filter(
+  (launch) => !isArchive(new Date(launch.launchWindow.endUTC))
+);
+const nextLaunch = futurLaunches.length == 0 ? null : futurLaunches[0];
+
+const formattedDate = new Intl.DateTimeFormat("fr-FR", {
+  timeZone: "Europe/Paris",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+}).format(new Date(nextLaunch.launchWindow.beginUTC));
+
+const formattedTime =
+  nextLaunch.launchWindow.beginUTC == nextLaunch.launchWindow.endUTC
+    ? null
+    : new Intl.DateTimeFormat("fr-FR", {
+        timeZone: "Europe/Paris",
+        hour: "numeric",
+        minute: "numeric",
+        // timeZoneName: "long"
+      }).format(new Date(nextLaunch.launchWindow.beginUTC));
+
+const nextLaunchTitle = nextLaunch
+  ? `Nouveau lancement: ${nextLaunch.title} le ${formattedDate}${
+      formattedTime ? " à " + formattedTime + " heure de Paris" : ""
+    }.`
+  : "Lancements de fusées, vaisseaux spatiaux ou événements spatiaux.";
+const nextLaunchDescription = nextLaunch ? `${nextLaunch.description}` : "";
+
 module.exports = {
   mode: "production",
   entry: "./src/js/index.js",
@@ -54,6 +90,32 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      meta: {
+        card_type: {
+          property: "og:type",
+          content: "website",
+        },
+        card_title: {
+          property: "og:title",
+          content: nextLaunchTitle,
+        },
+        card_description: {
+          property: "og:description",
+          content: nextLaunchDescription,
+        },
+        card_image: {
+          property: "og:image",
+          content: "https://space.magicorp.fr/thumb.png",
+        },
+        card_image_type: {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        card_url: {
+          property: "og:url",
+          content: "https://space.magicorp.fr/",
+        },
+      },
       minify: {
         collapseWhitespace: true,
         removeComments: true,
